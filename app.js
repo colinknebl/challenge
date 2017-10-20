e8082dcce5704fea4a9d83bae5fc6650af2ff2bd
Colin Knebl <colin.knebl@outlook.com>
2017-10-19 20:09:22 -0400

    done

---
+++ app.js
@@ -0,0 +1,32 @@
+var btn = document.getElementById('btn');
+
+var modalContainer = document.getElementById('modal-container');
+
+btn.addEventListener('click', function(){
+
+  if (modalContainer.classList.contains('hide')) {
+    modalContainer.classList.remove('hide');
+  } else {
+    modalContainer.classList.add('hide');
+  }
+
+});
+
+
+var arrayBtn = document.getElementById('array-btn');
+var output = document.getElementById('output');
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var newArray = [];
+
+arrayBtn.addEventListener('click', function() {
+
+  for (var i = 2; i < array.length; i += 3) {
+
+    newArray.push(array[i]);
+  }
+
+output.innerText = newArray;
+
+});
+
+
